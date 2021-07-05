840297eae2bd4273804b900bbc793d259c406f7c
Riya Lohia <riyalh1997@gmail.com>
2021-07-05 14:40:05 +0530

    Updates

---
+++ precache-manifest.6ae018f12497273ac579c071a7e019a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c93f1dd7079b3787eb95",
+    "url": "/z-vehica/static/css/main.626d3f8b.chunk.css"
+  },
+  {
+    "revision": "c93f1dd7079b3787eb95",
+    "url": "/z-vehica/static/js/main.c93f1dd7.chunk.js"
+  },
+  {
+    "revision": "4e3cca57d2e984b6e407",
+    "url": "/z-vehica/static/js/1.4e3cca57.chunk.js"
+  },
+  {
+    "revision": "e896ba3064e89b396f22",
+    "url": "/z-vehica/static/js/runtime~main.e896ba30.js"
+  },
+  {
+    "revision": "7e416beffb70ba1633fe589bb5abec21",
+    "url": "/z-vehica/static/media/video.7e416bef.mp4"
+  },
+  {
+    "revision": "5fb86cce2effdfee4d91396061b1c565",
+    "url": "/z-vehica/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/z-vehica/precache-manifest.ce910ef7d917f1de38411c1ef656a496.js"
+  "/z-vehica/precache-manifest.6ae018f12497273ac579c071a7e019a9.js"
 );
 
 workbox.clientsClaim();
